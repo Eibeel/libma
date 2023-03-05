6300811783f5c1862ee161c1e2c4913b5d39540d
Eibeel <abelguardop@gmail.com>
2023-03-05 15:01:12 -0500

    add: new libraries

--- src/libraries.js
+++ src/libraries.js
@@ -4,19 +4,42 @@ export const LIBRARIES = {
     installer: '@chakra-ui/react @emotion/react @emotion/styled framer-motion'
   },
   'Material UI': {
-    description: 'Componentes intuitivos y completamente cargados.',
+    description: 'de Google, componentes intuitivos y completamente cargados.',
     installer: '@mui/material @emotion/react @emotion/styled'
   },
-  PrimeReact: {
+  Tailwind: {
+    description: 'Marco para CSS, preparado para cualquier diseño css-in-js.',
+    installer: 'tailwindcss'
+  },
+  'Ant Design': {
+    description: 'Diseñe con componentes de clase empresarial y soporte de idiomas.'
+  },
+  'Prime React': {
     description: 'Componentes enriquecidos para React.',
     installer: 'primereact primeicons'
   },
   Mantine: {
-    description: 'Componentes para React con todas las funciones.',
+    description: 'Componentes para React lleno de funciones.',
     installer: '@mantine/core @mantine/hooks @emotion/react'
   },
+  NextUI: {
+    description: 'Hermosa, rapida y moderna. Se integra muy bien con Next.js.',
+    installer: '@nextui-org/react'
+  },
   HeaddlesUI: {
-    description: 'Componentes que se integran a la perfección con Tailwind CSS.',
+    description: 'Preparado para integrarse a la perfección con Tailwind CSS.',
     installer: '@headlessui/react'
+  },
+  Evergreen: {
+    description: 'Cree diseños ambiciosos en la web.',
+    installer: 'evergreen-ui'
+  },
+  'Semantic UI': {
+    description: 'Cree diseños utilizando HTML amigable basado en clases.',
+    installer: 'semantic-ui'
+  },
+  'Fluent UI': {
+    description: 'de Microsoft, componentes robustos y accesibles.',
+    installer: '@fluentui/react'
   }
 }
