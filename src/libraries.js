5087bf27ee50b4a361e11191473ab16076731a1b
Eibeel <abelguardop@gmail.com>
2023-03-08 13:39:39 -0500

    add: docs

--- src/libraries.js
+++ src/libraries.js
@@ -1,45 +1,57 @@
 export const LIBRARIES = {
   'Chakra UI': {
     description: 'Simple, modular y accesible.',
-    installer: '@chakra-ui/react @emotion/react @emotion/styled framer-motion'
+    installer: '@chakra-ui/react @emotion/react @emotion/styled framer-motion',
+    docs: 'https://chakra-ui.com/getting-started'
   },
   'Material UI': {
     description: 'de Google, componentes intuitivos y completamente cargados.',
-    installer: '@mui/material @emotion/react @emotion/styled'
+    installer: '@mui/material @emotion/react @emotion/styled',
+    docs: 'https://mui.com/material-ui/getting-started/overview/'
   },
   Tailwind: {
     description: 'Marco para CSS, preparado para cualquier diseño css-in-js.',
-    installer: 'tailwindcss'
+    installer: 'tailwindcss',
+    docs: 'https://tailwindcss.com/docs/installation'
   },
   'Ant Design': {
-    description: 'Diseñe con componentes de clase empresarial y soporte de idiomas.'
+    description: 'Diseñe con componentes de clase empresarial y soporte de idiomas.',
+    installer: 'antd',
+    docs: 'https://ant.design/components/overview/'
   },
   'Prime React': {
     description: 'Componentes enriquecidos para React.',
-    installer: 'primereact primeicons'
+    installer: 'primereact primeicons',
+    docs: 'https://primereact.org/configuration/'
   },
   Mantine: {
     description: 'Componentes para React lleno de funciones.',
-    installer: '@mantine/core @mantine/hooks @emotion/react'
+    installer: '@mantine/core @mantine/hooks @emotion/react',
+    docs: 'https://mantine.dev/pages/getting-started/'
   },
   NextUI: {
     description: 'Hermosa, rapida y moderna. Se integra muy bien con Next.js.',
-    installer: '@nextui-org/react'
+    installer: '@nextui-org/react',
+    docs: 'https://nextui.org/docs/guide/getting-started'
   },
   HeaddlesUI: {
     description: 'Preparado para integrarse a la perfección con Tailwind CSS.',
-    installer: '@headlessui/react'
+    installer: '@headlessui/react',
+    docs: 'https://headlessui.com/'
   },
   Evergreen: {
     description: 'Cree diseños ambiciosos en la web.',
-    installer: 'evergreen-ui'
+    installer: 'evergreen-ui',
+    docs: 'https://evergreen.segment.com/introduction/getting-started'
   },
   'Semantic UI': {
     description: 'Cree diseños utilizando HTML amigable basado en clases.',
-    installer: 'semantic-ui'
+    installer: 'semantic-ui',
+    docs: 'https://semantic-ui.com/introduction/getting-started.html'
   },
   'Fluent UI': {
     description: 'de Microsoft, componentes robustos y accesibles.',
-    installer: '@fluentui/react'
+    installer: '@fluentui/react',
+    docs: 'https://developer.microsoft.com/en-us/fluentui#/get-started/web'
   }
 }
